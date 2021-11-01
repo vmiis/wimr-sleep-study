163b30a9ec0577b62594d1be97f25e9649e0ff68
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-11-01 16:18:02 +1100

    update

---
+++ modules/module-list.js
@@ -0,0 +1,10 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p=""; //put a unique prefix to avoid conflict with others 
+    var H=$vm.hosting_path+"/modules";
+    var m=$vm.module_list;
+    var api="wimr";
+    //-------------------------------------------------------------------------------------
+    //m[p+"email-to-patient"]={url:H+"/business/email-to-patient/page.html",prefix:p,router:1};
+    //m[p+"clinic-email-automation"]={url:H+"/business/clinic-email-automation/page.html",prefix:p,router:1};
+})();
