d09546942b4dc61d40d8c4c3dddf7a01a53ae3da
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-11-04 09:32:36 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -5,10 +5,11 @@
     var m=$vm.module_list;
     var api="wimr";
     //-------------------------------------------------------------------------------------
-    m[p+"sleep-study-in-a-period"]      ={url:H+"/business/sleep-study-in-a-period/page.html",prefix:p,router:1};
-    m[p+"sleep-study-2022-average"]     ={url:H+"/business/sleep-study-2022-average/page.html",prefix:p,router:1};
-    m[p+"sleep-study-2021-average"]     ={url:H+"/business/sleep-study-2021-average/page.html",prefix:p,router:1};
-    m[p+"sleep-study-done-but-not-sent"]={url:H+"/business/sleep-study-done-but-not-sent/page.html",prefix:p,router:1};
-    m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/business/number-of-sleep-study-by-year-and-month/page.html",prefix:p,router:1};
-    m[p+"number-of-sleep-study-by-year"]={url:H+"/business/number-of-sleep-study-by-year/page.html",prefix:p,router:1};
+    m[p+"sleep-study-in-a-period-2"]    ={url:H+"/tasks/sleep-study-in-a-period-2/page.html",prefix:p,router:1};
+    m[p+"sleep-study-in-a-period"]      ={url:H+"/tasks/sleep-study-in-a-period/page.html",prefix:p,router:1};
+    m[p+"sleep-study-2022-average"]     ={url:H+"/tasks/sleep-study-2022-average/page.html",prefix:p,router:1};
+    m[p+"sleep-study-2021-average"]     ={url:H+"/tasks/sleep-study-2021-average/page.html",prefix:p,router:1};
+    m[p+"sleep-study-done-but-not-sent"]={url:H+"/tasks/sleep-study-done-but-not-sent/page.html",prefix:p,router:1};
+    m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/tasks/number-of-sleep-study-by-year-and-month/page.html",prefix:p,router:1};
+    m[p+"number-of-sleep-study-by-year"]={url:H+"/tasks/number-of-sleep-study-by-year/page.html",prefix:p,router:1};
 })();
